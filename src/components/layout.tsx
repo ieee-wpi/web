16a50637147815796040a60827761d3627957439
daniel <38931252+dgorbunov@users.noreply.github.com>
2024-12-12 13:28:03 -0500

    fix footer header case

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+import Footer from "./footer";
+import Navbar from "./navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
